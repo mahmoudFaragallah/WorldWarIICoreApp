b8f37b8ef9a1e657a0aba3a27ea69e642c1265a8
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2020-06-10 22:11:59 +0200

    modify connectionString

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WorldWarIIApp.Data;
 using Microsoft.EntityFrameworkCore;
+
 namespace WebApp
 {
     public class Startup
@@ -17,9 +14,8 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            services.AddDbContext<WorldWarIIContext>(
-                option => option.UseSqlServer("DefaultConnection"));
+            services.AddDbContext<WorldWarIIContext>(option => 
+                option.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WorldWarIIDb;Trusted_Connection=True;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
